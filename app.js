8814a10b947609f2f8c7240f0def3ed18599bd1a
mik9016 <mikolajgruszecki@o2.pl>
2021-02-07 12:23:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const { func } = require('prop-types');
+const functions = require('./functions');
+
+console.log('Hello World');
+
+
+functions.helloWorld();
+functions.add(1,2);
---
+++ functions.js
@@ -0,0 +1,7 @@
+const helloWorld = () => console.log('hello')
+const add = (a,b) => {console.log(a+b)} 
+
+module.exports = {
+    helloWorld,
+    add
+}
